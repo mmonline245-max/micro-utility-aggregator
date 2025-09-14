0893c037fa527c5f1a5909086ee31e495ec1faf9
mmonline245-max <mmonline245@gmail.com>
2025-09-14 19:50:49 -0400

    Create AdSlot.tsx

---
+++ components/AdSlot.tsx
@@ -0,0 +1,8 @@
+export default function AdSlot() {
+  return (
+    <div className="my-6 border rounded-lg bg-gray-100 text-center p-6 text-sm text-gray-500">
+      {/* Replace with AdSense script later */}
+      Ad Placeholder
+    </div>
+  );
+}
